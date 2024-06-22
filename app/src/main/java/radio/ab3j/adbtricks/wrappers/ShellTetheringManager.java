dc016ac078504a37860d5db8bcb46532d4c13e1f
A. C. Buglione <andrea@buglione.eu>
2024-06-21 23:01:47 -0400

    and another

--- app/src/main/java/radio/ab3j/adbtricks/wrappers/ShellTetheringManager.java
+++ app/src/main/java/radio/ab3j/adbtricks/wrappers/ShellTetheringManager.java
@@ -6,34 +6,44 @@ import org.apache.commons.lang3.reflect.MethodUtils;
 import com.genymobile.scrcpy.FakeContext;
 
 import android.net.LinkAddress;
+import android.os.IBinder;
+
+import java.util.function.Supplier;
 
 public final class ShellTetheringManager {
 
-    private static final Class REQUEST_BUILDER_CLASS;
+    private static final String SERVICE_NAME = android.content.Context.TETHERING_SERVICE;
 
-    static {
-        try {
-            REQUEST_BUILDER_CLASS = Class.forName("android.net.TetheringManager$TetheringRequest$Builder");
-        } catch (ReflectiveOperationException e) {
-            throw new AssertionError(e);
-        }
-    }
+    private static final String MANAGER_CLASS = "android.net.TetheringManager";
 
     private final Object mService;
 
     static ShellTetheringManager create() {
-        Object service = FakeContext.get().getSystemService("tethering");
+        try {
+            IBinder b = ShellServiceManager.getService(SERVICE_NAME);
 
-        return new ShellTetheringManager(service);
+            Object manager = ConstructorUtils.invokeConstructor(Class.forName(MANAGER_CLASS), FakeContext.get(), new Supplier<IBinder>() {
+                @Override
+                public IBinder get() {
+                    return b;
+                }
+            });
+
+            return new ShellTetheringManager(manager);
+        } catch (ReflectiveOperationException e) {
+            throw new AssertionError(e);
+        }
     }
 
     private ShellTetheringManager(Object service) {
         this.mService = service;
     }
 
+    private static final String REQUEST_BUILDER_CLASS = "android.net.TetheringManager$TetheringRequest$Builder";
+
     public void startTethering(int type) {
         try {
-            Object builder = ConstructorUtils.invokeConstructor(REQUEST_BUILDER_CLASS, type);
+            Object builder = ConstructorUtils.invokeConstructor(Class.forName(REQUEST_BUILDER_CLASS), type);
 
             Object request = MethodUtils.invokeMethod(builder, "build");
 
@@ -45,7 +55,7 @@ public final class ShellTetheringManager {
 
     public void startTethering(int type, LinkAddress localIPv4Address, LinkAddress clientAddress) {
         try {
-            Object builder = ConstructorUtils.invokeConstructor(REQUEST_BUILDER_CLASS, type);
+            Object builder = ConstructorUtils.invokeConstructor(Class.forName(REQUEST_BUILDER_CLASS), type);
 
             MethodUtils.invokeMethod(builder, "setStaticIpv4Addresses", localIPv4Address, clientAddress);
 
